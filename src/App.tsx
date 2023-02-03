8569706778b46c3a7a43878ca930bec5c8092cd9
Diogo Soares Peixoto <71359547+Diogo-Peixoto@users.noreply.github.com>
2023-02-03 17:47:55 -0300

    eventos

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,82 @@
-import { Stage, Layer, Rect, Circle, Text, Line, Shape } from "react-konva";
+import { useState } from "react";
+import {
+  Stage,
+  Layer,
+  Rect,
+  Circle,
+  Text,
+  Line,
+  Shape,
+  Star,
+} from "react-konva";
+
+function generateShapes() {
+  return [...Array(3)].map((_, i) => ({
+    id: i.toString(),
+    x: Math.random() * window.innerWidth,
+    y: Math.random() * window.innerHeight,
+    rotation: Math.random() * 180,
+    isDragging: false,
+  }));
+}
 
 function App() {
+  const [stars, setStars] = useState(generateShapes());
+
+  const handleDragStart = (e: any) => {
+    const id = e.target.id();
+    setStars(
+      stars.map((star) => {
+        return {
+          ...star,
+          isDragging: star.id === id, // mesma coise de ferificar a strela igual
+        };
+      })
+    );
+  };
+
+  const handleDragEnd = (e: any) => {
+    console.log(e);
+    setStars(
+      stars.map((star) => {
+        return {
+          ...star,
+          isDragging: false,
+        };
+      })
+    );
+  };
+
   return (
-    <>
-      <Stage width={window.innerWidth} height={window.innerHeight}>
-        <Layer x={400}>
-          <Text text="Formas padrão" />
-          <Rect x={20} y={50} width={100} height={100} fill="red" />
-          <Circle x={200} y={100} fill="green" radius={50} />
-          <Line
-            x={20}
-            y={200}
-            stroke="black"
-            points={[0, 0, 100, 0, 100, 100]}
-            tension={0.5}
-            closed
-            fillLinearGradientStartPoint={{ x: -50, y: -50 }}
-            fillLinearGradientEndPoint={{ x: 50, y: 50 }}
-            fillLinearGradientColorStops={[0, "red", 1, "yellow"]}
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer>
+        <Text text="Try to drag a star" />
+        {stars.map((star) => (
+          <Star
+            key={star.id}
+            id={star.id}
+            x={star.x}
+            y={star.y}
+            numPoints={5}
+            innerRadius={20}
+            outerRadius={40}
+            fill="#89b717"
+            opacity={0.8}
+            draggable
+            rotation={star.rotation}
+            shadowColor="black"
+            shadowBlur={10}
+            shadowOpacity={0.6}
+            shadowOffsetX={star.isDragging ? 10 : 5}
+            shadowOffsetY={star.isDragging ? 10 : 5}
+            scaleX={star.isDragging ? 1.2 : 1}
+            scaleY={star.isDragging ? 1.2 : 1}
+            onDragStart={handleDragStart}
+            onDragEnd={handleDragEnd}
           />
-        </Layer>
-        <Layer>
-          <Shape
-            sceneFunc={(context, shape) => {
-              context.beginPath();
-              context.moveTo(20, 50);
-              context.lineTo(220, 80);
-              context.quadraticCurveTo(150, 100, 260, 170);
-              context.closePath();
-              context.fillStrokeShape(shape);
-            }}
-            fill="#00D2FF"
-            stroke="black"
-            strokeWidth={4}
-          />
-        </Layer>
-      </Stage>
-    </>
+        ))}
+      </Layer>
+    </Stage>
   );
 }
 
