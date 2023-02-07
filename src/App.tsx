c2161a252586311d6602a2dd938e20af9efe194a
Diogo Soares Peixoto <71359547+Diogo-Peixoto@users.noreply.github.com>
2023-02-07 20:07:37 -0300

    Movento um nó para outro Camada

--- src/App.tsx
+++ src/App.tsx
@@ -9,27 +9,49 @@ import {
   Text,
   Circle,
   Transformer,
+  Group,
+  Line,
 } from "react-konva";
 import useImage from "use-image";
-import { Html } from "react-konva-utils";
+import { Portal } from "react-konva-utils";
 
 function App() {
+  const [isDragging, setDragging] = useState(false);
+
   return (
     <Stage width={window.innerWidth - 20} height={window.innerHeight - 20}>
       <Layer>
-        <Html
-          divProps={{
-            style: {
-              position: "absolute",
-              top: 10,
-              left: 10,
-            },
-          }}
-        >
-          <input placeholder="Estou dentro do canvas" />
-        </Html>
-        <Rect x={20} y={20} width={50} height={50} fill="red" shadowBlur={5} />
+        <Portal selector=".top-layer" enabled={isDragging}>
+          <Rect
+            x={20}
+            y={50}
+            width={150}
+            height={150}
+            fill="red"
+            draggable
+            onDragStart={() => {
+              setDragging(true);
+            }}
+            onDragEnd={() => {
+              setDragging(false);
+            }}
+          />
+        </Portal>
+        <Circle x={200} y={100} radius={50} fill="green" />
+        <Line
+          x={20}
+          y={200}
+          points={[0, 0, 100, 0, 100, 100]}
+          tension={0.5}
+          closed
+          stroke="black"
+          fillLinearGradientStartPoint={{ x: -50, y: -50 }}
+          fillLinearGradientEndPoint={{ x: 50, y: 50 }}
+          fillLinearGradientColorStops={[0, "red", 1, "yellow"]}
+          draggable
+        />
       </Layer>
+      <Layer name="top-layer" />
     </Stage>
   );
 }
