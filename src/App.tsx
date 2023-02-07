694a66ea0fdd8e0061b2b2214a66f96b7d9ba383
Diogo Soares Peixoto <71359547+Diogo-Peixoto@users.noreply.github.com>
2023-02-07 16:20:03 -0300

    animação simples

--- src/App.tsx
+++ src/App.tsx
@@ -11,77 +11,27 @@ import {
   Transformer,
 } from "react-konva";
 import useImage from "use-image";
-import Rectangle from "./components/Rectangle";
-
-export interface IPropsOnchange {
-  fill: string;
-  height: number;
-  rotation: number;
-  id: string;
-  width: number;
-  x: number;
-  y: number;
-}
-
-const initialRectangles = [
-  {
-    x: 10,
-    y: 10,
-    width: 100,
-    height: 100,
-    rotation: 0,
-    fill: "red",
-    id: "rect1",
-  },
-  {
-    x: 150,
-    y: 150,
-    width: 100,
-    height: 100,
-    rotation: 0,
-    fill: "green",
-    id: "rect2",
-  },
-];
+import { KonvaEventListener } from "konva/lib/Node";
 
 function App() {
-  const [rectangles, setRectangles] = useState(initialRectangles);
-  const [selectedId, selectShape] = useState(null);
-  console.log(rectangles);
-  const checkDeselect = (e: any) => {
-    const clickedOnEmpty = e.target === e.target.getStage();
-    if (clickedOnEmpty) {
-      selectShape(null);
-    }
+  const changeSize = (e: any) => {
+    e.target.to({
+      scaleX: Math.random() + 0.8,
+      scaleY: Math.random() + 0.8,
+      duration: 0.2,
+    });
   };
 
   return (
-    <Stage
-      width={window.innerWidth}
-      height={window.innerHeight}
-      onMouseDown={checkDeselect}
-      onTouchStart={checkDeselect}
-    >
+    <Stage width={window.innerWidth - 20} height={window.innerHeight - 20}>
       <Layer>
-        {rectangles.map((rect: any, index) => {
-          return (
-            <Rectangle
-              key={index}
-              shapeProps={rect}
-              isSelected={rect.id === selectedId}
-              onSelect={() => {
-                selectShape(rect.id);
-              }}
-              //Responsavel por trocar as novas informações pelas antigas
-              // Recebe os valores da onchange vinda do <Rectangle>
-              onChange={(newAttrs: IPropsOnchange) => {
-                const rects = rectangles.slice();
-                rects[index] = newAttrs;
-                setRectangles(rects);
-              }}
-            />
-          );
-        })}
+        <Rect
+          draggable
+          onDragStart={changeSize}
+          fill="green"
+          width={100}
+          height={100}
+        />
       </Layer>
     </Stage>
   );
